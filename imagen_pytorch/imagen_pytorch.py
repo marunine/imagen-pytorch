25ed60116047906bb5e14dcaaf8ca114dc704181
marunine <97926137+marunine@users.noreply.github.com>
2022-06-15 21:23:29 -0400

    update default heads to 32
    extended dimension does not seem to be necessary for custom t5

--- imagen_pytorch/imagen_pytorch.py
+++ imagen_pytorch/imagen_pytorch.py
@@ -389,7 +389,7 @@ class PerceiverAttention(nn.Module):
         self,
         *,
         dim,
-        dim_head = 64,
+        dim_head = 32,
         heads = 8
     ):
         super().__init__()
@@ -446,7 +446,7 @@ class PerceiverResampler(nn.Module):
         *,
         dim,
         depth,
-        dim_head = 64,
+        dim_head = 32,
         heads = 8,
         num_latents = 64,
         num_latents_mean_pooled = 4, # number of latents derived from mean pooled representation of the sequence
@@ -500,7 +500,7 @@ class Attention(nn.Module):
         self,
         dim,
         *,
-        dim_head = 64,
+        dim_head = 32,
         heads = 8,
         context_dim = None
     ):
@@ -711,7 +711,7 @@ class CrossAttention(nn.Module):
         dim,
         *,
         context_dim = None,
-        dim_head = 64,
+        dim_head = 32,
         heads = 8,
         norm_context = False
     ):
