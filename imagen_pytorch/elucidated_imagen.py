fix loss calculation - match k-diffusion

imagen_pytorch/elucidated_imagen.py
@@ -542,27 +542,23 @@ class ElucidatedImagen(nn.Module):
 
         # get prediction
 
-        denoised_images = self.preconditioned_network_forward(
-            unet.forward,
-            noised_images,
-            sigmas,
-            sigma_data = sigma_data,
+        net_out = unet.forward(
+            self.c_in(sigma_data, padded_sigmas) * noised_images,
+            self.c_noise(sigmas),
             text_embeds = text_embeds,
             text_mask = text_masks,
             cond_images = cond_images,
-            lowres_noise_times = self.lowres_noise_schedule.get_condition(lowres_aug_times),
             lowres_cond_img = lowres_cond_img_noisy,
+            lowres_noise_times = self.lowres_noise_schedule.get_condition(lowres_aug_times),
             cond_drop_prob = self.cond_drop_prob,
         )
 
         # losses
 
-        losses = F.mse_loss(denoised_images, images, reduction = 'none')
+        target = (images - self.c_skip(sigma_data, padded_sigmas) * noised_images) / self.c_out(sigma_data, padded_sigmas)
+
+        losses = F.mse_loss(net_out, target, reduction = 'none')
         losses = reduce(losses, 'b ... -> b', 'mean')
-
-        # loss weighting
-
-        losses = losses * self.loss_weight(sigma_data, sigmas)
 
         # return average loss
 